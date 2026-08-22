96fe0b0899c506523da7e93032ac341a941db5ed
Jorge Devia <workwithdevia@gmail.com>
2026-08-22 15:50:50 -0500

    chore(podman): clean up configuration by removing commented lines and unused settings

--- modules/nixos/virtualisation/podman.nix
+++ modules/nixos/virtualisation/podman.nix
@@ -1,14 +1,7 @@
 {
   virtualisation.podman = {
     enable = true;
-
-    # Crea un alias en el sistema para que el comando 'docker' ejecute 'podman'
     dockerCompat = true;
-
-    # Habilita la resolución de nombres DNS entre contenedores (muy útil para podman-compose)
     defaultNetwork.settings.dns_enabled = true;
-
-    # Habilita la red por defecto de podman (podman network create podman)
-    defaultNetwork.enable = true;
   };
 }
