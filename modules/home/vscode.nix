1f095993881c41a25d64169591253c5bbb366077
Jorge Devia <workwithdevia@gmail.com>
2026-09-03 14:47:07 -0500

    fix(vscode): update sha256

--- modules/home/vscode.nix
+++ modules/home/vscode.nix
@@ -5,7 +5,7 @@
       src = builtins.fetchTarball {
         url = "https://code.visualstudio.com/sha/download?build=insider&os=linux-x64";
         # Colocamos un placeholder temporal para que Nix falle y nos dé el hash correcto en consola
-        sha256 = "0arb4ncx6pi9yia6a24q2gn4gp4ns9wbmc57hjwaza3r08abig65";
+        sha256 = "0b2v97dip1z4lv2blrpqnfs2daxybpn7shng6kwa3m56h94kvmh9";
       };
       version = "latest";
 
